ac7394f893be5e00927d3f0f8f09b53871c8e17c
pozhilan15 <pozhilanjj95@gmail.com>
2020-05-26 20:26:09 +0530

    second commit

--- src/gitproject/org/Git.java
+++ src/gitproject/org/Git.java
@@ -1,5 +1,10 @@
 package gitproject.org;
 
-public class Git {
-
-}
+class GFG { 
+    public static void main(String[] args) 
+    { 
+        System.out.println("Welcome"); 
+        System.out.println("To"); 
+        System.out.println("GeeksforGeeks"); 
+    } 
+} 
